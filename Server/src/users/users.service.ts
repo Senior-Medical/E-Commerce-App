edeb5392db9358600b33cc919d20f13a391b0e32
Mohamed Twfik <mohamedtwfik910@gmail.com>
2024-11-22 20:13:59 +0200

    set encryption service in common folder

--- Server/src/users/users.service.ts
+++ Server/src/users/users.service.ts
@@ -4,13 +4,19 @@ import { User } from "./entities/users.entity";
 import { InjectModel } from "@nestjs/mongoose";
 import { CreateUsersDto } from "./dtos/createUser.dto";
 import * as bcrypt from "bcrypt";
+import { EncryptionService } from '../common/services/encryption.service';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class UsersService {
-  constructor(@InjectModel(User.name) private usersModel: Model<User>) { }
+  constructor(
+    @InjectModel(User.name) private usersModel: Model<User>,
+    private readonly configService: ConfigService
+  ) { }
   
   async comparePassword(password: string, hashedPassword: string) {
-    return await bcrypt.compare(password, hashedPassword);
+    const encryptionService = new EncryptionService(this.configService);
+    return encryptionService.bcryptCompare(password, hashedPassword);
   }
 
   find(condition: any) {
