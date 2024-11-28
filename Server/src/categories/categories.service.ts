a47d9080cff328c9c5abde61a34f5a637731d0c2
Mohamed Twfik <mohamedtwfik910@gmail.com>
2024-11-28 18:44:27 +0200

    review and add comments for category module

--- Server/src/categories/categories.service.ts
+++ Server/src/categories/categories.service.ts
@@ -5,20 +5,45 @@ import { Category } from "./entities/categories.entity";
 import { CreateCategoryDto } from "./dtos/creatCategory.dto";
 import { UpdateCategoryDto } from "./dtos/updateCategory.dto";
 
+/**
+ * Service class that handles business logic for categories, such as creating, 
+ * updating, finding, and deleting categories. It ensures categories have unique 
+ * names before performing creation or updates. 
+ */
 @Injectable()
 export class CategoriesServices{
   constructor(
     @InjectModel(Category.name) private categoriesModel: Model<Category>
   ) { }
 
+  /**
+   * Finds categories based on specified conditions.
+   * 
+   * @param conditions - The search conditions.
+   * @returns List of categories that match the conditions.
+   */
   find(conditions: object = {}) {
     return this.categoriesModel.find(conditions).select("-__v");
   }
 
+  /**
+   * Finds a category by its ID.
+   * 
+   * @param id - The ID of the category.
+   * @returns The category document.
+   */
   findOne(id: string) {
     return this.categoriesModel.findById(id).select("-__v");
   }
 
+  /**
+   * Creates a new category if it does not already exist.
+   * 
+   * @param categoryData - Data for the new category.
+   * @param user - The user creating the category.
+   * @throws ConflictException if the category already exists.
+   * @returns The created category.
+   */
   async create(categoryData: CreateCategoryDto, user: Document) {
     const category = (await this.find({ name: categoryData.name }))[0];
     if (category) throw new ConflictException('Category already exist.');
@@ -32,6 +57,15 @@ export class CategoriesServices{
     return this.categoriesModel.create(inputData);
   }
 
+  /**
+   * Updates an existing category if the name is unique.
+   * 
+   * @param category - The category document to be updated.
+   * @param categoryData - Data to update the category.
+   * @param user - The user performing the update.
+   * @throws ConflictException if the new category name already exists.
+   * @returns The updated category document.
+   */
   async update(category: Document, categoryData: UpdateCategoryDto, user: Document) {
     let categoryByName = (await this.find({ name: categoryData.name }))[0];
     if (categoryByName && categoryByName._id.toString() != category._id.toString()) throw new ConflictException('Category already exist.');
@@ -43,6 +77,12 @@ export class CategoriesServices{
     return category.set(inputData).save();
   }
 
+  /**
+   * Deletes a category.
+   * 
+   * @param category - The category document to be deleted.
+   * @returns The result of the delete operation.
+   */
   remove(category: Document) {
     return category.deleteOne();
   }
--- Server/src/categories/dtos/updateCategory.dto.ts
+++ Server/src/categories/dtos/updateCategory.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from "@nestjs/mapped-types"
 import { CreateCategoryDto } from "./creatCategory.dto"
 
+/**
+ * Data transfer object used for updating an existing category. It extends 
+ * the `CreateCategoryDto` and makes all fields optional to allow partial 
+ * updates to the category.
+ */
 export class UpdateCategoryDto extends PartialType(CreateCategoryDto) { }
